0b509a5c5ab0adc1ff46ff48a0db4bc9c5d3f4e6
rakibpaholan <rakibpaholan9999@gmail.com>
2021-02-20 02:06:42 +0600

    Auto Complete Text View Practice Updated 0.1

--- app/src/main/java/com/example/autocompletetextviewpractice/SplashActivity.java
+++ app/src/main/java/com/example/autocompletetextviewpractice/SplashActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 public class SplashActivity extends AppCompatActivity {
@@ -14,6 +15,9 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         progressBar = (ProgressBar)findViewById(R.id.progressBarId);
         Thread thread  = new Thread(new Runnable() {
             @Override
