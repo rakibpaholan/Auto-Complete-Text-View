463e8f10c36db10912025781961b0e0d37099408
rakibpaholan <rakibpaholan9999@gmail.com>
2021-02-19 15:44:28 +0600

    Auto Complete Text View Practice Updated

--- app/src/main/java/com/example/autocompletetextviewpractice/MainActivity.java
+++ app/src/main/java/com/example/autocompletetextviewpractice/MainActivity.java
@@ -5,8 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
    private AlertDialog.Builder alertDialogBuilder;
@@ -22,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,country_name);
         autoCompleteTextView.setThreshold(1);
         autoCompleteTextView.setAdapter(adapter);
+
+       autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+               String value = autoCompleteTextView.getText().toString();
+               Toast.makeText(getApplicationContext(),value,Toast.LENGTH_SHORT).show();
+           }
+       });
 
 
     }
